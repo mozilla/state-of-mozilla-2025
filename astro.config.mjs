f6dd9581e858e9a5d1b2f5ed781dcb4cbae2ac8e
countfak <marco@tilt.computer>
2026-01-30 11:19:19 +0100

    sitemap, robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import tailwindcss from "@tailwindcss/vite";
 
 import svelte from "@astrojs/svelte";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://stateof.mozilla.org",
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [svelte()],
+  integrations: [svelte(), sitemap()],
 });
