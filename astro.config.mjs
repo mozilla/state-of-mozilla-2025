acf2d668f091a7bbc3e5a6ea6727788bda55aaed
countfak <marco@tilt.computer>
2026-01-30 11:15:05 +0100

    site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,9 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://stateof.mozilla.org",
   vite: {
     plugins: [tailwindcss()],
   },
-
   integrations: [svelte()],
 });
